b6e524022186880064f7a02ccaa1e77eae5559c2
tycoon87 <tyson.woodruff@outlook.com>
2017-10-09 13:21:16 -0700

    working

--- portfolio.py
+++ portfolio.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template
 
-app = Flask(__name__)   
+app = Flask(__name__)
 
 @app.route('/')
-def page():
-    return render_template("index.html")                                   
-app.run(debug=True)     
+def index():
+    return render_template('index.html')
+
+@app.route('/projects')
+def projects():
+    my_projects = ['stuff', 'things Iv Done', 'Appropreate things to say', 'stop being a dipp stick']
+    return render_template('projects.html', projects=my_projects)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+app.run(debug=True)
